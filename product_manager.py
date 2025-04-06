8018fe43a11bb8843b35e2c34b9ea51a57afffb0
minabambina <minas.scekic1@gmail.com>
2025-04-06 12:08:32 +0200

    Added remove_product to remove product by name

--- product_manager.py
+++ product_manager.py
@@ -14,3 +14,16 @@ class ProductManager:
     def total_inventory_value(self):
         total_value = sum(product.price * product.quantity for product in self.products)
         return total_value
+    
+    def remove_product(self, product_name):
+        product_to_remove = None
+        for product in self.products:
+            if product.name.lower() == product_name.lower():
+                product_to_remove = product
+                break
+
+        if product_to_remove:
+            self.products.remove(product_to_remove)
+            print(f"Product '{product_name}' has been removed.")
+        else:
+            print(f"Product '{product_name}' not found.")
